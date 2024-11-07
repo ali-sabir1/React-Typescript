fd2b26d81d3b5e3219b921e0083ba48eab437609
Ali Sabir <alisabir0075@gmail.com>
2024-11-07 14:25:50 +0500

    A simple TODO app using React and Typescript

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,31 @@
+import {useRef, useContext} from 'react';
+import { TodosContext } from '../store/todos-context';
+import classes from "./NewTodo.module.css"
+
+const NewTodo: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (e: React.FormEvent)=>{
+        e.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        if(enteredText.trim().length === 0){
+            // Throw Error
+            return;
+        }
+
+        todosCtx.addTodo(enteredText);
+    }
+    return(
+        <form onSubmit={handleSubmit} className={classes.form}>
+            <label htmlFor="text">Todo Text</label>
+            <input type="text" id="text" ref={todoTextInputRef}/>
+            <button>Add Todo</button>
+        </form>
+    )
+}
+
+export default NewTodo;
